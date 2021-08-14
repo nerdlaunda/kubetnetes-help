a42de04075859cc1fbce1e9bd5049d3f6f7cb8ba
Nerd <nerdlaunda@gmail.com>
2021-08-14 17:58:27 +0530

    setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
+echo 'Starting Setup' 
+
+echo 'Configuaring .bashrc'
 # add alias to bashrc
 curl https://raw.githubusercontent.com/nerdlaunda/kubetnetes-helper/main/.bashrc >> $HOME/.bashrc
 source $HOME/.bashrc
 
+echo 'Configuating .vimrc'
 # add tab specifications to .vimrc
 echo "set tabstop=2" >> $HOME/.vimrc
 echo "set expandtab" >> $HOME/.vimrc
