d42a4209f0116d07a507365cc14f37f7f1179797
Nerd <nerdlaunda@gmail.com>
2021-08-14 17:49:25 +0530

    add vimrc

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ curl https://raw.githubusercontent.com/nerdlaunda/kubetnetes-helper/main/.bashrc
 source $HOME/.bashrc
 
 # add tab specifications to .vimrc
+echo "set tabstop=2" >> $HOME/.vimrc
+echo "set expandtab" >> $HOME/.vimrc
+echo "set shiftwidth=2" >> $HOME/.vimrc
