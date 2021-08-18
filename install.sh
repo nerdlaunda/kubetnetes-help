cda97f009499f04ac1b7d7ec6e60a6225a665ed8
Nerd <nerdlaunda@gmail.com>
2021-08-19 03:06:58 +0530

    asthethic

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
-echo 'Starting Setup' 
+echo '[-> ] Starting Setup' 
 
-echo 'Configuating .bashrc'
+echo '[ ! ] Configuating .bashrc'
 sleep .5
-echo 'Creating backup of ~/.bashrc to ~/.bashrc-bak-nl-k8s'
+echo '[ ! ] Creating backup of ~/.bashrc to ~/.bashrc-bak-nl-k8s'
 cp ~/.bashrc ~/.bashrc-bak-nl-k8s
 
-echo 'Downloading and Configuaring .bashrc'
+echo '[!!!] Downloading and Configuaring .bashrc'
 # add alias to bashrc
 if [ -f "~/.bashrc-bak-nl-k8s" ]; then
-    echo 'Backup file already exist. '
+    echo '[!!!] Backup file already exist. '
     responce="n"
-    read -p "Do you want to continue by overwriting it?[y/N]: " -n 1 -r responce
+    read -p "[ ? ] Do you want to continue by overwriting it?[y/N]: " -n 1 -r responce
     echo    
     if [[ $responce =~ ^[Yy]$ ]]
     then
@@ -18,31 +18,32 @@ if [ -f "~/.bashrc-bak-nl-k8s" ]; then
     fi
     sleep .5
 fi
+cp ~/.bashrc ~/.bashrc-bak-nl-k8s
 curl -s https://raw.githubusercontent.com/nerdlaunda/kubetnetes-helper/main/.bashrc >> $HOME/.bashrc
 source $HOME/.bashrc
-echo '~/.bashrc is updated.'
+echo '[-> ] ~/.bashrc is updated.'
 sleep .5
 
-echo 'Configuating .vimrc'
+echo '[ ! ] Configuating .vimrc'
 # add customization to .vimrc
-echo 'Creating backup...'
+echo '[ ! ] Creating backup...'
 if [ -f "~/.vimrc" ]; then
-    echo '.vimrc exists.'
-    echo 'Creating a backup of ~/.vimrv to ~/.vimrc-bak-nl-k8s'
+    echo '[ ! ] .vimrc exists.'
+    echo '[ ! ]Creating a backup of ~/.vimrv to ~/.vimrc-bak-nl-k8s'
     cp ~/.vimrc ~/.vimrc-bak-nl-k8s
 else
-    echo "~/.vimrv doesn't exists. Creating new file."
+    echo "[!!!] ~/.vimrv doesn't exists. Creating new file."
     touch ~/.vimrc
     touch ~/.vimrc-bak-nl-k8s
 fi
-echo 'Downloading and Configuaring .vimrc'
+echo '[ ! ] Downloading and Configuaring .vimrc'
 curl -s https://raw.githubusercontent.com/nerdlaunda/kubetnetes-helper/main/.vimrc >> $HOME/.vimrc
 sleep .5
 
 
 # COMPLETE -run bash
-echo "Complete!!! Starting bash."
+echo "[-> ] Complete!!! Starting bash."
 sleep .5
 exec bash
 source $HOME/.bashrc
-echo "Installation complete. Try to type k and hit enter. If output is kubectl help then you're good to go. Other wise type 'exec bash'."
+echo "[ ! ]Installation complete. Try to type k and hit enter. If output is kubectl help then you're good to go. Other wise type 'exec bash'."
