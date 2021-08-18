bc45b6c918b0781f38832e5e5c2d6b7e1314e4f2
Nerd <nerdlaunda@gmail.com>
2021-08-19 03:08:15 +0530

    asthertic

--- uninstall.sh
+++ uninstall.sh
@@ -1,28 +1,28 @@
-echo 'Begin Uninstall...'
+echo '[-> ] Begin Uninstall...'
 
-echo 'Replacing ~/.bashrc with ~/.bashrc-bak-nl-k8s'
+echo '[ ! ] Replacing ~/.bashrc with ~/.bashrc-bak-nl-k8s'
 sleep .5
 if [ -f "~/.bashrc-bak-nl-k8s" ]; then
-    echo 'Replacing ~/.bashrc with ~/.bashrc-bak-nl-k8s'
+    echo '[ ! ] Replacing ~/.bashrc with ~/.bashrc-bak-nl-k8s'
     mv ~/.bashrc-bak-nl-k8s ~/.bashrc
     rm ~/.bashrc-bak-nl-k8s
     sleep .5
 else
-    echo 'Backup for .bashrc is not available.'
-    echo '!!!  Manual cleaning required. Edit ~/.bashrc and remove everything between "#CUSTOM-ALIAS-END-K8S" and "#CUSTOM-ALIAS-END-K8S"'
-    echo 'Skipping...'
+    echo '[ ! ] Backup for .bashrc is not available.'
+    echo '[!!!]  Manual cleaning required. Edit ~/.bashrc and remove everything between "#CUSTOM-ALIAS-END-K8S" and "#CUSTOM-ALIAS-END-K8S"'
+    echo '[-> ] Skipping...'
 fi
 source $HOME/.bashrc
 
 if [ -f "~/.vimrc-bak-nl-k8s" ]; then
-    echo 'Replacing ~/.vimrc with ~/.vimrc-bak-nl-k8s'
+    echo '[ ! ] Replacing ~/.vimrc with ~/.vimrc-bak-nl-k8s'
     mv ~/.vimrc-bak-nl-k8s ~/.vimrc
     rm ~/.vimrc-bak-nl-k8s
     sleep .5
 else
-    echo 'Backup for .bashrc is not available.'
-    echo 'Removing ~/.vimrc'
+    echo '[ ! ] Backup for .bashrc is not available.'
+    echo '[ ! ] Removing ~/.vimrc'
     rm ~/.vimrc
 fi
 
-echo "Installation complete. Try to type 'k' and hit enter. If output is 'kubectl help' then type 'exec bash'."
+echo "[-> ] Installation complete. Try to type 'k' and hit enter. If output is 'kubectl help' then type 'exec bash'."
